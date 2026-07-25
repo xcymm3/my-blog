0d1cfbd9562ed5c9f8f2e9b87593e9cdc8ee7162
xcy <xiang.congyuan@oceantek.pro>
2026-07-25 15:43:59 +0800

    chore: configure github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {/* config options here */};
+const repoName = "my-blog";
+const isGithubPagesBuild = process.env.GITHUB_ACTIONS === "true";
+
+const nextConfig: NextConfig = {
+  basePath: isGithubPagesBuild ? `/${repoName}` : "",
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
+  trailingSlash: true,
+};
 
 export default nextConfig;
